09cae391252e955380e977cc089880ce1991bd70
Karolina <kara1805@gmail.com>
2018-12-14 12:18:49 +0100

    first controller

---
+++ src/main/java/pl/abc/myspringdemo/controller/MyController.java
@@ -0,0 +1,13 @@
+package pl.abc.myspringdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MyController {
+    @GetMapping({"/","/home"})
+    public  String welcome()
+    {
+        return "welcome";
+    }
+}
